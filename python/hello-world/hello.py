1bc3ed270b1d8886dec4dd489816be6d2555cb10
Verlin Grace Cabalhug <cabalhugverlin@gmail.com>
2023-08-14 18:07:04 +0800

    Added Python Practice Room

---
+++ python/hello-world/hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+print(100)
+print(1 + 7)
+print(9 - 10)
+print(9 * 8)
+print(7 / 7)
+
+print('Hello', end=' | ')
+print(100, end=' | ')
---
+++ python/variables/variables.py
@@ -0,0 +1,10 @@
+name = 'Verlin'
+numberOfCandies = 16
+
+print('Hello, ' + name)
+print(f'She ate {numberOfCandies} candies')
+
+
+msg = 10
+msg = 'this is a string'
+print(msg)
